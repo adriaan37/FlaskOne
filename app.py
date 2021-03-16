f7170d7ef3159f6e9f16c9e1cfc0e6e32ecffe29
Stephan Venter <Stephan.Venter@techtorium.ac.nz>
2021-03-16 15:39:30 +1300

    Errors fixed

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
+"""This is the main flask app file"""
 from flask import Flask, render_template
 import names
 
 app = Flask(__name__)
 
-#Generate random names
-def GenerateNames(numberofnames):
+def generate_names(numberofnames):
+    """Generate random names"""
     nlist = []
     for i in range(numberofnames):
         nlist.append(names.get_full_name())
+        print(i)
 
     return nlist
 
-namelist = GenerateNames(50)
+namelist = generate_names(50)
 
-#Homepage
 @app.route('/')
 def home():
+    """Render Homepage"""
     return render_template('home.html', namelist=namelist)
